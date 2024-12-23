ae5c6dceced823a9d8d3c4225a6276cabfe07cf7
Aleksey <podlesnovav91@gmail.com>
2024-12-23 09:45:26 +0300

    Update module7.2.py

--- module7.2.py
+++ module7.2.py
@@ -1,40 +1,20 @@
-info = [
-    'Text for tell.',
-
-    'Используйте кодировку utf-8.',
-
-    'Because there are 2 languages!',
-
-    'Спасибо!'
-
-    ]
-
 def custom_write(file_name,string):
     strings_positions={}
     n=0
-    for i in string:
-        with open(file_name, "a" ,encoding= "utf-8")as number: #а+ открытие и дозапись
+    for line in string:
+        with open(file_name, "a+" ,encoding= "utf-8")as number: #а+ открытие и дозапись
             p=number.tell()
-            number.write(i+"\n")
+            number.write(line+"\n")
         n+=1
-        strings_positions.update({(n,p):i})
+        strings_positions.update({(n,p):line})
     return strings_positions
 info = [
-
     'Text for tell.',
-
     'Используйте кодировку utf-8.',
-
     'Because there are 2 languages!',
-
     'Спасибо!'
-
     ]
 
-
-
 result = custom_write('test.txt', info)
-
 for elem in result.items():
-
-  print(elem)
+    print(elem)
